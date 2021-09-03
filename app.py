22892213f1ad699291da9ed45beb612216e02f10
Dennis Chmielewski <chmielewski_dennis@hotmail.com>
2021-09-03 13:28:45 +0000

    push all code before meeting

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -23,6 +24,11 @@ def get_books():
     books = list(mongo.db.books.find())
     print(books)
     return render_template("books.html", books=books)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
