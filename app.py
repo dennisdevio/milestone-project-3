354dae0b6132b3760587d5fddc032ddc520f70db
Dennis Chmielewski <chmielewski_dennis@hotmail.com>
2021-09-04 19:08:09 +0000

    add logout function and redirct users to login page

--- app.py
+++ app.py
@@ -87,6 +87,19 @@ def profile(username):
         {"username": session["user"]})["username"]
     return render_template("profile.html", username=username)
 
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user's session cookies
+    flash("You are now logged out")
+    session.pop("user")
+    return. redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
