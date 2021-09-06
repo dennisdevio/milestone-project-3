c7271ba6a7b6cab0081d3c66f00f73c05b75da48
Dennis Chmielewski <chmielewski_dennis@hotmail.com>
2021-09-06 03:56:31 +0000

    add buttons to review posts

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,6 @@
-/*var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))*/
+var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
 
-/*
 var collapseList = collapseElementList.map(function (collapseEl) {
   return new bootstrap.Collapse(collapseEl)
 })
-*/
+
